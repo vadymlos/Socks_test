cc54edfd7684723a07d6b476026a03654fbda11f
vadymlos <vadym.los@pdffiller.team>
2020-05-01 20:06:44 +0300

    Initial Commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.CollectionCondition.*;
+
+
+public class Test1 {
+
+    @Test(description = "Registration")
+
+    public void firstTestMethod(){
+
+        open("http://localhost/index.html");
+
+        $("#register").click();
+//        $("#Login").shouldHave(text("Register"));
+//        $x("//div[@class='modal-title' and contains(text(),'Register')]").shouldHave(text("Register"));
+        $("#register-username-modal").setValue("Tester1");
+        $("#register-first-modal").setValue("Vadym1");
+        $("#register-last-modal").setValue("Los1");
+        $("#register-email-modal").setValue("mem1@gmail.com");
+        $("#register-password-modal").setValue("autotest");
+
+        $x("//button[.=' Register\n" + "                            ']").click();
+
+        $x("//a[text()='Logged in as Vadym1 Los1']").shouldHave(text("Logged in as Vadym1 Los1"));
+        $x("//a[text()='Logout']").shouldHave(text("Logout"));
+        $("#numItemsInCart").shouldHave(text("0 items in cart"));
+    }
+}
