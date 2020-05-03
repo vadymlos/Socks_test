97ed1d7a9ec2afb96ef1063db7fb0e23770201fa
vadymlos <vadym.los@pdffiller.team>
2020-05-03 20:30:48 +0300

    Added_Login

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -15,20 +15,23 @@ public class Test1 {
 
     public void firstTestMethod(){
 
+        String usename = "Tester";
+
         open("http://localhost/index.html");
 
         $("#register").click();
-//        $("#Login").shouldHave(text("Register"));
-//        $x("//div[@class='modal-title' and contains(text(),'Register')]").shouldHave(text("Register"));
-        $("#register-username-modal").setValue("Tester2");
-        $("#register-first-modal").setValue("Vadym1");
-        $("#register-last-modal").setValue("Los1");
-        $("#register-email-modal").setValue("mem1@gmail.com");
+        $("#register-modal").shouldHave(text("Register"));
+
+        $("#register-username-modal").setValue("Tester8");
+        $("#register-first-modal").setValue("Vadym5");
+        $("#register-last-modal").setValue("Los5");
+        $("#register-email-modal").setValue("mem7@gmail.com");
         $("#register-password-modal").setValue("autotest");
+//        $(byText("Tester5")).shouldBe(visible);
 
         $x("//button[.=' Register\n" + "                            ']").click();
 
-        $x("//a[text()='Logged in as Vadym1 Los1']").shouldHave(text("Logged in as Vadym1 Los1"));
+        $x("//a[text()='Logged in as Vadym5 Los5']").shouldHave(text("Logged in as Vadym5 Los5"));
         $x("//a[text()='Logout']").shouldHave(text("Logout"));
         $("#numItemsInCart").shouldHave(text("0 items in cart"));
     }
@@ -39,12 +42,12 @@ public class Test1 {
 
         $x("//a[text()='Logout']").shouldHave(text("Logout")).click();
         $x("//a[text()='Login']").shouldHave(text("Login")).click();
-        $("#username-modal").setValue("Tester2");
+        $("#username-modal").setValue("Tester8");
         $("#password-modal").setValue("autotest");
 
         $x("//button[.=' Log in\n" + "                            ']").click();
 
-        $x("//a[text()='Logged in as Vadym1 Los1']").shouldHave(text("Logged in as Vadym1 Los1"));
+        $x("//a[text()='Logged in as Vadym5 Los5']").shouldHave(text("Logged in as Vadym5 Los5"));
         $x("//a[text()='Logout']").shouldHave(text("Logout"));
         $("#numItemsInCart").shouldHave(text("0 items in cart"));
 
