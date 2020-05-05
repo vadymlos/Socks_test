bcd018dc731dc82858953ac8f128a84eda46be3c
vadymlos <vadym.los@pdffiller.team>
2020-05-05 19:34:59 +0300

    Some change2FromDen

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,7 +1,10 @@
+import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.Date;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -11,25 +14,30 @@ import static com.codeborne.selenide.CollectionCondition.*;
 
 public class Test1 {
 
+    String username = "Vadym" + new Date().getTime();
+
     @Test(description = "Registration")
 
     public void firstTestMethod(){
 
-        String usename = "Tester";
 
         open("http://localhost/index.html");
 
         $("#register").click();
-        $("#register-modal").shouldHave(text("Register"));
+//        $("#register-modal").shouldHave(text("Register"));
+        $("#register-modal").waitUntil(text("Register"), 5000);
+        $x("//div[@id='register-modal']//button[@class='btn btn-primary']").waitUntil(visible, 5000);
 
-        $("#register-username-modal").setValue("Tester8");
+
+        $("#register-username-modal").click();
+        $("#register-username-modal").sendKeys(username);
         $("#register-first-modal").setValue("Vadym5");
         $("#register-last-modal").setValue("Los5");
         $("#register-email-modal").setValue("mem7@gmail.com");
         $("#register-password-modal").setValue("autotest");
 //        $(byText("Tester5")).shouldBe(visible);
 
-        $x("//button[.=' Register\n" + "                            ']").click();
+        $x("//div[@id='register-modal']//button[@class='btn btn-primary']").click();
 
         $x("//a[text()='Logged in as Vadym5 Los5']").shouldHave(text("Logged in as Vadym5 Los5"));
         $x("//a[text()='Logout']").shouldHave(text("Logout"));
@@ -42,7 +50,9 @@ public class Test1 {
 
         $x("//a[text()='Logout']").shouldHave(text("Logout")).click();
         $x("//a[text()='Login']").shouldHave(text("Login")).click();
-        $("#username-modal").setValue("Tester8");
+        $("#username-modal").click();
+        Selenide.sleep(2000);
+        $("#username-modal").sendKeys(username);
         $("#password-modal").setValue("autotest");
 
         $x("//button[.=' Log in\n" + "                            ']").click();
